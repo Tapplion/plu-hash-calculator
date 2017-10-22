80da93b125e735a95bd9436f801b25ba942a1ae8
Pascal Kimmel <development@tapplion.com>
2017-10-22 11:31:51 +0200

    Initial Commit

---
+++ PLU-Hash-Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PLU-Hash-Calculator
+//
+//  Created by Pascal Kimmel on 22-10-17.
+//  Copyright © 2017 Pascal Kimmel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
