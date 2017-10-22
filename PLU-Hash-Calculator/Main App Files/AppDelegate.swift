bfc806ef197d67aed640dd740dc2d6f62b1653cb
Pascal Kimmel <development@tapplion.com>
2017-10-22 11:36:41 +0200

    Project setup

---
+++ PLU-Hash-Calculator/Main App Files/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  PLU-Hash-Calculator
+//
+//  Created by Pascal Kimmel on 22-10-17.
+//  Copyright © 2017 Pascal Kimmel. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
